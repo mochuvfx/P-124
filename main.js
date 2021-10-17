8787c943850509acec280a96fa1ae875c084a65e
mochuvfx <87718949+mochuvfx@users.noreply.github.com>
2021-10-17 12:26:11 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+noseX = 0;
+noseY = 0;
 difference = 0;
-rightWristX = 0;
-leftWristX = 0;
+rightwristX = 0;
+leftwristX = 0;
 
 function setup() {
     video = createCapture(VIDEO);
@@ -10,7 +12,7 @@ function setup() {
     canvas.position(560, 150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotposes);
+    poseNet.on('pose', gotPoses);
 }
 
 
@@ -22,10 +24,25 @@ function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
-        rightWristX = results[0].pose.rightwrist.x;
-        leftWristX = results[0].pose.leftwrise.x;
-        difference = floor(leftWristX - rightWristX);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
 
-        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
+        leftwristX = results[0].pose.leftwrist.x;
+        rightwristX = results[0].pose.rightwrist.x;
+        difference = floor(leftwristX - rightwristX);
+        
+        console.log("leftwristX = " + leftwristX + "rightwristX" + rightwristX + "difference = " + difference);
+
     }
 }
+
+function draw() {
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "width and height of the square will be = " + difference + "px";
+    fill('#00FFCD');
+    stroke('#00FFCD');
+    square(noseX, noseY, difference);
+}
+
